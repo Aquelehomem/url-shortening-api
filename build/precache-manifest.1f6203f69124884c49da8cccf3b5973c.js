367cefb113e1a21127f3606fa6f8f39b7b974ea5
Aquelehomem <gustavomartinsdp@outlook.com>
2020-03-28 17:35:59 -0300

    commit

---
+++ build/precache-manifest.1f6203f69124884c49da8cccf3b5973c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032a12414f5a0305f196a04c6b13fc17",
+    "url": "/url-shortening-api/index.html"
+  },
+  {
+    "revision": "3fa41ddd68e644853e79",
+    "url": "/url-shortening-api/static/css/main.b96fe439.chunk.css"
+  },
+  {
+    "revision": "07f09b32e54cf1495c41",
+    "url": "/url-shortening-api/static/js/2.29b2e02d.chunk.js"
+  },
+  {
+    "revision": "fb53afac904a19f752339ed5143ffede",
+    "url": "/url-shortening-api/static/js/2.29b2e02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa41ddd68e644853e79",
+    "url": "/url-shortening-api/static/js/main.5dd23a46.chunk.js"
+  },
+  {
+    "revision": "13aad364c975a9569b72",
+    "url": "/url-shortening-api/static/js/runtime-main.a6a9000a.js"
+  }
+]);
